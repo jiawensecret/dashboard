c12912ebc0ac13d9b6dc022ae0812b1ec81309cc
piaohua <294128448@qq.com>
2019-10-11 17:35:11 +0800

    first commit

--- resources/admin/pages/rbac/permission.vue
+++ resources/admin/pages/rbac/permission.vue
@@ -1,34 +1,6 @@
 <template>
     <div class="app-container">
         <div class="filter-container">
-            <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item"
-                      @keyup.enter.native="handleFilter"/>
-            <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px"
-                       class="filter-item">
-                <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item"/>
-            </el-select>
-            <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px">
-                <el-option v-for="item in calendarTypeOptions" :key="item.key"
-                           :label="item.display_name+'('+item.key+')'" :value="item.key"/>
-            </el-select>
-            <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
-                <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key"/>
-            </el-select>
-            <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-                Search
-            </el-button>
-            <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit"
-                       @click="handleCreate">
-                Add
-            </el-button>
-            <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download"
-                       @click="handleDownload">
-                Export
-            </el-button>
-            <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;"
-                         @change="tableKey=tableKey+1">
-                reviewer
-            </el-checkbox>
         </div>
 
         <el-table
@@ -41,64 +13,49 @@
             style="width: 100%;"
             @sort-change="sortChange"
         >
-            <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80"
+            <el-table-column label="id" prop="id" sortable="custom" align="center" width="80"
                              :class-name="getSortClass('id')">
                 <template slot-scope="scope">
                     <span>{{ scope.row.id }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="Date" width="150px" align="center">
+            <el-table-column label="名称" width="150px" align="center">
                 <template slot-scope="scope">
-                    <span>{{ scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+                    <span>{{ scope.row.alias_name}}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="Title" min-width="150px">
-                <template slot-scope="{row}">
-                    <span class="link-type" @click="handleUpdate(row)">{{ row.title }}</span>
-                    <el-tag>{{ row.type | typeFilter }}</el-tag>
-                </template>
-            </el-table-column>
-            <el-table-column label="Author" width="110px" align="center">
+            <el-table-column label="分类" min-width="150px">
                 <template slot-scope="scope">
-                    <span>{{ scope.row.author }}</span>
+                    <span>{{ scope.row.guard_name}}</span>
                 </template>
             </el-table-column>
-            <el-table-column v-if="showReviewer" label="Reviewer" width="110px" align="center">
+            <el-table-column label="路由" width="150px" align="center">
                 <template slot-scope="scope">
-                    <span style="color:red;">{{ scope.row.reviewer }}</span>
+                    <span>{{ scope.row.name }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="Imp" width="80px">
+            <el-table-column label="路径" width="150px" align="center">
                 <template slot-scope="scope">
-                    <svg-icon v-for="n in +scope.row.importance" :key="n" icon-class="star" class="meta-item__icon"/>
+                    <span>{{ scope.row.uri }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="Readings" align="center" width="95">
-                <template slot-scope="{row}">
-                    <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
-                    <span v-else>0</span>
+            <el-table-column label="创建时间" width="150px" align="center">
+                <template slot-scope="scope">
+                    <span>{{ scope.row.created_at }}</span>
                 </template>
             </el-table-column>
-            <el-table-column label="Status" class-name="status-col" width="100">
-                <template slot-scope="{row}">
-                    <el-tag :type="row.status | statusFilter">
-                        {{ row.status }}
-                    </el-tag>
+            <el-table-column label="修改时间" width="150px" align="center">
+                <template slot-scope="scope">
+                    <span>{{ scope.row.updated_at }}</span>
                 </template>
             </el-table-column>
+
             <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
                 <template slot-scope="{row}">
                     <el-button type="primary" size="mini" @click="handleUpdate(row)">
                         Edit
                     </el-button>
-                    <el-button v-if="row.status!='published'" size="mini" type="success"
-                               @click="handleModifyStatus(row,'published')">
-                        Publish
-                    </el-button>
-                    <el-button v-if="row.status!='draft'" size="mini" @click="handleModifyStatus(row,'draft')">
-                        Draft
-                    </el-button>
-                    <el-button v-if="row.status!='deleted'" size="mini" type="danger"
+                    <el-button size="mini" type="danger"
                                @click="handleModifyStatus(row,'deleted')">
                         Delete
                     </el-button>
@@ -112,30 +69,17 @@
         <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
             <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px"
                      style="width: 400px; margin-left:50px;">
-                <el-form-item label="Type" prop="type">
-                    <el-select v-model="temp.type" class="filter-item" placeholder="Please select">
-                        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name"
-                                   :value="item.key"/>
-                    </el-select>
+                <el-form-item label="名称" prop="alias_name">
+                    <el-input v-model="temp.alias_name"/>
                 </el-form-item>
-                <el-form-item label="Date" prop="timestamp">
+                <el-form-item label="分类" prop="type">
                     <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date"/>
                 </el-form-item>
-                <el-form-item label="Title" prop="title">
-                    <el-input v-model="temp.title"/>
+                <el-form-item label="路由" prop="route">
+                    <el-input v-model="temp.name"/>
                 </el-form-item>
-                <el-form-item label="Status">
-                    <el-select v-model="temp.status" class="filter-item" placeholder="Please select">
-                        <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item"/>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="Imp">
-                    <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3"
-                             style="margin-top:8px;"/>
-                </el-form-item>
-                <el-form-item label="Remark">
-                    <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea"
-                              placeholder="Please input"/>
+                <el-form-item label="路径" prop="path">
+                    <el-input v-model="temp.uri"/>
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
@@ -181,15 +125,8 @@
                 listQuery: {
                     page: 1,
                     limit: 20,
-                    importance: undefined,
-                    title: undefined,
-                    type: undefined,
                     sort: '+id'
                 },
-                importanceOptions: [1, 2, 3],
-                calendarTypeOptions,
-                sortOptions: [{label: 'ID Ascending', key: '+id'}, {label: 'ID Descending', key: '-id'}],
-                statusOptions: ['published', 'draft', 'deleted'],
                 showReviewer: false,
                 temp: {
                     id: undefined,
@@ -197,8 +134,7 @@
                     remark: '',
                     timestamp: new Date(),
                     title: '',
-                    type: '',
-                    status: 'published'
+                    type: ''
                 },
                 dialogFormVisible: false,
                 dialogStatus: '',
@@ -207,11 +143,10 @@
                     create: 'Create'
                 },
                 dialogPvVisible: false,
-                pvData: [],
                 rules: {
-                    type: [{required: true, message: 'type is required', trigger: 'change'}],
-                    timestamp: [{type: 'date', required: true, message: 'timestamp is required', trigger: 'change'}],
-                    title: [{required: true, message: 'title is required', trigger: 'blur'}]
+                    type: [{ required: true, message: 'type is required', trigger: 'change' }],
+                    timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+                    title: [{ required: true, message: 'title is required', trigger: 'blur' }]
                 },
                 downloadLoading: false
             }
@@ -336,12 +271,6 @@
                 })
                 const index = this.list.indexOf(row)
                 this.list.splice(index, 1)
-            },
-            handleFetchPv(pv) {
-                fetchPv(pv).then(response => {
-                    this.pvData = response.data.pvData
-                    this.dialogPvVisible = true
-                })
             },
             handleDownload() {
                 this.downloadLoading = true
