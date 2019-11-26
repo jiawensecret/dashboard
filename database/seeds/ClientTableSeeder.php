125cf85b3ec2a7dd56057e64aad5349db38b42ea
Gone with the wind <294128448@qq.com>
2019-11-26 12:00:53 +0800

    dbseed

--- database/seeds/ClientTableSeeder.php
+++ database/seeds/ClientTableSeeder.php
@@ -16,6 +16,8 @@ class ClientTableSeeder extends Seeder
             'secret' => 'zO5znG43MAvr75AvSu3HhnnuRMesskKuxtKLT86V',
             'redirect' => env('APP_URL',''),
             'password_client' => 1,
+            'personal_access_client' => 0,
+            'revoked' => 0,
             'created_at' => date('Y-m-d H:i:s')
         ]);
     }
